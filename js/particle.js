71f2f03b21c65d6ad0ed279892b26ea88de9f5f3
SuperSak <potturusaketh@gmail.com>
2021-01-04 19:36:05 -0800

    wow

--- js/particle.js
+++ js/particle.js
@@ -8,6 +8,7 @@ class Particle{
         this.body = Bodies.circle(x, y, r, options)
         this.color = color(random(0,255), random(0,255), random(0,255));
         World.add(world, this.body)
+        this.idk=idk
     }
     display(){
       /*  var angle = this.body.angle;
@@ -21,12 +22,36 @@ class Particle{
         ellipse(0, 0, this.r, this.r)
         pop()*/
 
-
         push()
         ellipseMode(RADIUS)
         fill(this.color)
         ellipse(this.body.position.x, this.body.position.y, this.r, this.r)
         pop()
-        
+        if(this.body.position.y>=480&&this.idk<1){
+            if(this.body.position.x<=100&&this.body.position.x>=0){
+                score+=100
+                this.idk++
+            }
+            if(this.body.position.x<=200&&this.body.position.x>=101){
+                score+=500
+                this.idk++
+            }
+            if(this.body.position.x<=300&&this.body.position.x>=201){
+                score+=300
+                this.idk++
+            }
+            if(this.body.position.x<=400&&this.body.position.x>=301){
+                score+=200
+                this.idk++
+            }
+            if(this.body.position.x<=500&&this.body.position.x>=401){
+                score+=0
+                this.idk++
+            }
+            if(this.body.position.x<=600&&this.body.position.x>=501){
+                score+=400
+                this.idk++
+            }
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -3,15 +3,23 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
-var ground;
-var particles=[]
-var plinkos=[]
-var divisions=[]
-var divisionHeight=225
+var ground, particle, over, oover;
+var idk=0
+var particles=[];
+var plinkos=[];
+var divisions=[];
+var divisionHeight=225;
+var score=0;
+var turn=0;
+gameState=0;
+function preload(){
+  oover=loadImage('download.png')
+}
 function setup() {
-  createCanvas(600,700);
   engine = Engine.create();
   world = engine.world;
+  createCanvas(600,700).mousePressed(mousePressed);
+  
   ground= new Ground(width/2, height-12.5,width,25);
   for(i=0; i<=width; i+=100){
     divisions.push(new Ground(i, height-divisionHeight/2, 10, divisionHeight))
@@ -32,11 +40,31 @@ function setup() {
   }
 }
 function draw() {
-  Engine.update(engine);
-
   background(0);  
-  if(frameCount%30==0){
+  Engine.update(engine);
+  console.log(mouseX);
+  push()
+  fill(255,100,0);
+  textSize(32)
+  text("Score: "+score, 20, 40)
+  pop()
+  push()
+  textSize(24)
+  fill(200)
+  text("100",30,510)
+  text("500",130,510)
+  text("300",230,510)
+  text("200",330,510)
+  text("0",440,510)
+  text("400",530,510)
+  pop()
+  /**if(frameCount%60==0){
     particles.push(new Particle(random(width/2-10, width/2+10),10,10))
+  }**/
+  if(turn>=5){
+    gameState=1
+    over=createSprite(width/2, height/2)
+    over.addImage(oover)
   }
   for(k=0; k<divisions.length; k++){
     divisions[k].display(255,255,255)
@@ -49,5 +77,12 @@ function draw() {
   }
   ground.display(255,255,255);
   drawSprites();
-  
+}
+function mousePressed(){
+  console.log("in Mouse")
+   if(gameState!==1){
+     turn++
+     particle=new Particle(mouseX, 10,10,10);
+     particles.push(particle)
+   }
 }
